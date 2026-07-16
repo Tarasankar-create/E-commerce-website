5f06d9072593cad4fbcbb05fcfd0798cf551ff2f
Tarasankar <dastarasankar393@gmail.com>
2026-07-16 13:52:35 +0530

    Remove cache files

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -78,11 +78,16 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME','mydatabase'),
+        'USER': os.getenv('DB_USER','root'),
+        'PASSWORD': os.getenv('DB_PASSWORD',''),
+        'HOST': os.getenv('DB_HOST','mysql'),
+        'PORT': os.getenv('DB_PORT','3306'),
     }
 }
 
