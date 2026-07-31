a5af8769f90a2af35f940ff221b1467d45744910
Tarasankar <dastarasankar393@gmail.com>
2026-07-31 22:52:09 +0530

    Update settings and application

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-9(34@)zwogbdl+)v=xcd$+$os=gsvf0-x1kawhnddwlugsp-we')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
